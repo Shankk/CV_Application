d958b35ff201e8282a282a038b037c3ddbf30985
Shankk <brianviveiros@hotmail.com>
2023-11-08 16:20:28 -0500

    Dynamic Education List Creation
    Can create new education and edit, cancel edits and/or even delete education items in said list which updates dynamicaly to the content.

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,45 +1,68 @@
 import DropDownMenu from "./DropDownMenu";
+import { ManageEducation, EducationList } from "./Education";
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
 import './Content.css'
+function Personal({name = "Brian Viveiros",email = "coolman123@gmail.com",
+    phone = "123-456-7890",address = "123 Fake Street"}) {
+
+    const personalStyle = {
+        color: "red",
+        fontSize: 12
+    };
+
+    return (
+        <div className="personalContent">
+            <h1>{name}</h1>
+            <p>{email} {phone} {address}</p>
+        </div>
+    )
+}
 
 function Content() {
+    //Personal States
+    const [name,setName] = useState('Matthew Williams');
+    const [email,setEmail] = useState('example@hotmail.com');
+    const [phone,setPhone] = useState('123-456-7890');
+    const [address,setAddress] = useState('123 Fake Street');
+    
+    // Education States
+    const [educationList, setEducationList] = useState([]);
+    const [school,setSchool] = useState('');
+    const [degree,setDegree] = useState('');
+    const [start,setStart] = useState('');
+    const [end,setEnd] = useState('');
+    const [location, setLocation] = useState('');
+    
+    const eduProps =  {school:school, degree:degree, start:start, end:end, location:location}
+    const eduMethods = {setSchool,setDegree,setStart,setEnd,setLocation}
+
     return (
         <div className="container">
             <div className="edits">
                 <DropDownMenu identity={"personal"} text={"Personal Details"}>
                     <p>Full Name</p>
-                    <input type="text" />
+                    <input type="text" placeholder="first and last name..." value={name} onChange={(event) => setName(event.target.value)}/>
                     <p>Email</p>
-                    <input type="email" />
+                    <input type="text" placeholder="email address" value={email} onChange={(event) => setEmail(event.target.value)}/>
                     <p>Phone number</p>
-                    <input type="number" />
+                    <input type="text" placeholder="phone number" value={phone} onChange={(event) => setPhone(event.target.value)}/>
                     <p>Address</p>
-                    <input type="text" />
+                    <input type="text" placeholder="address" value={address} onChange={(event) => setAddress(event.target.value)}/>
                 </DropDownMenu>
                 <DropDownMenu identity={"education"} text={"Education"}>
-                    <p>School</p>
-                    <input type="text" />
-                    <p>Degree</p>
-                    <input type="text" />
-                    <div>
-                        <p>Start Date</p>
-                        <input type="date" />
-                        <p>End Date</p>
-                        <input type="date" />
-                    </div>
-                    <p>Location</p>
-                    <input type="text" />
+                       <ManageEducation list={educationList} setList={setEducationList} eduProps={eduProps} eduMethods={eduMethods}>
+                       </ManageEducation>
                 </DropDownMenu>
                 <DropDownMenu identity={"experience"} text={"Experience"}>
                     <p>Company Name</p>
                     <input type="text" />
                     <p>Position Title</p>
                     <input type="text" />
-                    <div>
-                        <p>Start Date</p>
-                        <input type="date" />
-                        <p>End Date</p>
-                        <input type="date" />
-                    </div>
+                    <p>Start Date</p>
+                    <input type="text" />
+                    <p>End Date</p>
+                    <input type="text" />
                     <p>Location</p>
                     <input type="text" />
                     <p>Description</p>
@@ -47,13 +70,13 @@ function Content() {
                 </DropDownMenu>
             </div>
             <div className="content">
-                <h1>Brian Viveiros</h1>
+                <Personal name= {name} email={email} phone={phone} address={address}>
+                </Personal>
+                <EducationList educationList={educationList}>
+                </EducationList>
             </div>
         </div>
     )
 }
-
-
-
 
 export default Content;
---
+++ src/Education.jsx
@@ -0,0 +1,189 @@
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+function Education({school = "George Brown",degree = "Game Programing",
+    start = "08/2018", end = "04/2021", location = "Toronto ON"}) {
+
+    const educationStyle = {
+        color: "red",
+        fontSize: 12
+    };
+
+    return (
+        <div className="educationContentItem">
+            <div className="contentItemGroup">
+                <p>{start} {end} </p>
+                <p>{location}</p>
+            </div>
+            <div className="contentItemGroup">
+                <h3>{school}</h3>
+                <p>{degree}</p>
+            </div>
+        </div>
+    )
+}
+
+function EducationList(props) {
+    if(!props.educationList) {
+        return (
+            <>
+                <h2>Education</h2>
+                <p>Trying to fetch Education...</p>
+            </>
+        )
+    }
+
+    if(props.educationList.length === 0) {
+        return (
+            <>
+                <h2>Education</h2>
+                <p>No Education has been added. It is reccomended to add some!</p>
+            </>
+        )
+    }
+
+    return (
+        <div className="educationContent">
+            <h2>Education</h2>
+            {props.educationList.map((education,i) => {
+                return <Education key={i} school={education.school} degree={education.degree} 
+                start={education.start} end={education.end} location={education.location} />;
+            })}
+        </div>
+    )
+}
+
+function ManageEducation({list, setList, eduProps, eduMethods}) {
+    const [expand, setExpand] = useState(false)
+    const [index, setIndex] = useState(0)
+    const [oldInput, setOldInput] = useState("")
+    const [isNew, setNew] = useState(false)
+    
+    if(list.length > 0){
+        //Updates List with input data
+        list[index].school = eduProps.school
+        list[index].degree = eduProps.degree
+        list[index].start = eduProps.start
+        list[index].end = eduProps.end
+        list[index].location = eduProps.location
+    }
+    const resetInputs = () => {
+        eduMethods.setSchool("")
+        eduMethods.setDegree("")
+        eduMethods.setStart("")
+        eduMethods.setEnd("")
+        eduMethods.setLocation("")
+    }
+    const loadInputs = (array,index) => {
+        //offset to allow copy of data ahead or behind in list.
+        let oldData = ""
+        if(index >= array.length) {
+            array.filter((item,i) => {
+                if(i == (index-1)) {
+                    oldData = {school:item.school, degree:item.degree, start:item.start, end:item.end, location:item.location}
+                    eduMethods.setSchool(item.school)
+                    eduMethods.setDegree(item.degree)
+                    eduMethods.setStart(item.start)
+                    eduMethods.setEnd(item.end)
+                    eduMethods.setLocation(item.location)
+                } 
+            })
+            setIndex(array.length-1)
+        }
+        else {
+            array.filter((item,i) => {
+                if(i == index) {
+                    oldData = {school:item.school, degree:item.degree, start:item.start, end:item.end, location:item.location}
+                    eduMethods.setSchool(item.school)
+                    eduMethods.setDegree(item.degree)
+                    eduMethods.setStart(item.start)
+                    eduMethods.setEnd(item.end)
+                    eduMethods.setLocation(item.location)
+                } 
+            })
+            setIndex(index)
+        }
+        return oldData
+    }
+    const handleNew = () => {
+        //Setup for New Item. we open inputs. push temp item in list.
+        setExpand(true)
+        setNew(true)
+        resetInputs()
+        let tempArr = list
+        tempArr.push(eduProps)
+        setList(tempArr)
+        setIndex((list.length-1))
+    }
+    const handleOld = (index) => {
+        //Setup for Existing Item. Loading existing data from list.
+        setOldInput(loadInputs(list,index))
+        setExpand(true);
+        setNew(false);
+    }
+    const handleSave = () => {
+        setExpand(false)
+        setNew(false)
+    }
+    const handleDelete = () => {
+        // We first filter the item we want to delete from the list
+        let temp = list.filter((item, i) => i !== index);
+        // Since List is shifted of the deleted item. we grab the newly indexed data
+        // to overwrite the current saved data we just deleted.
+        // Reason because is that once we re-render. the index we were on will try to load
+        // the current state data when we decide to edit one of our list items.
+        loadInputs(temp,index)
+        setList(temp)
+        setExpand(false);
+    }
+    const handleCancel = () => {
+        if(isNew == true) { // if new Item. just delete.
+            let temp = list.filter((item, i) => i !== index);
+            loadInputs(temp,index)
+            setList(temp)
+        }
+        else {
+            eduMethods.setSchool(oldInput.school)
+            eduMethods.setDegree(oldInput.degree)
+            eduMethods.setStart(oldInput.start)
+            eduMethods.setEnd(oldInput.end)
+            eduMethods.setLocation(oldInput.location)
+        }
+        setExpand(false);
+        
+    }
+    
+    if(expand) {
+        return (
+            <>
+                <p>School</p>
+                <input type="text" name="school" value={eduProps.school} onChange={(event) => eduMethods.setSchool(event.target.value)}/>
+                <p>Degree</p>
+                <input type="text" name="degree" value={eduProps.degree} onChange={(event) => eduMethods.setDegree(event.target.value)}/>
+                <p>Start</p>
+                <input type="text" name="start_date" value={eduProps.start} onChange={(event) => eduMethods.setStart(event.target.value)}/>
+                <p>End</p>
+                <input type="text" name="end_date" value={eduProps.end} onChange={(event) => eduMethods.setEnd(event.target.value)}/>
+                <p>Location</p>
+                <input type="text" name="location" value={eduProps.location} onChange={(event) => eduMethods.setLocation(event.target.value)}/>
+                <div>
+                    <button onClick={() => handleDelete()}>Delete</button>
+                    <button onClick={() => handleCancel()}>Cancel</button>
+                    <button onClick={() => handleSave()}>Save</button>
+                </div>
+            </>
+        )
+    }
+    else {
+        return (
+            <>
+                {list.length > 0 && list.map((item, i) => {
+                    return <button key={i} onClick={() => handleOld(i)}>{item.school}</button>
+                })}
+                <button onClick={handleNew}>Add New Education</button>
+            </>
+        )
+    } 
+}
+
+export {ManageEducation,EducationList}
